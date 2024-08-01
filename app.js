ff2b1cd3886f83599c0736cd0343f46f2d449013
Sameer Ahmed <balochtradersnayapay@gmail.com>
2024-08-01 11:25:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+var istatus = document.querySelector("h4");
+var btn = document.querySelector("#btn");
+var flag = 0
+
+btn.addEventListener("click", function () {
+  if (flag == 0) {
+    istatus.innerHTML = "Friend";
+    istatus.style.color = "green";
+    btn.innerHTML = "Remove Friend";
+    btn.style.backgroundColor = "red"
+    flag = 1
+  }
+  else {
+    istatus.innerHTML = "Stranger";
+    istatus.style.color = "red";
+    btn.innerHTML = "Add Friend";
+    btn.style.color = "white"
+    btn.style.backgroundColor = "blue"
+    flag = 0
+  }
+});
+
